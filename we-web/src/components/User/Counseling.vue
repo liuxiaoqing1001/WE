0f55c07222cc881968950b2c241f93c8ea286075
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2021-03-08 23:33:32 +0800

    20210308更新1

--- we-web/src/components/User/Counseling.vue
+++ we-web/src/components/User/Counseling.vue
@@ -1,10 +1,36 @@
 <template>
-<div>sss</div>
+  <div style="margin-top: 100px">
+
+
+
+
+    <div style="border: 1px solid gray">
+      <shrink-view v-model="open">
+
+        <img src="../../assets/1.jpeg" height="640" width="1024"/>
+      </shrink-view>
+      <input type="checkbox" v-model="open">
+    </div>
+
+  </div>
 </template>
 
 <script>
+  import ShrinkView from '../../components/User/ShrinkView';
     export default {
-        name: "Counseling"
+        name: "Counseling",
+
+
+      components: {
+        ShrinkView
+      },
+      data () {
+        return {
+          open: false
+        }
+      }
+
+
     }
 </script>
 

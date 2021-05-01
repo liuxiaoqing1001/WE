32719c2e5e63b2459b73c765e1525e85e5411f1b
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2021-05-02 01:26:45 +0800

    20210502更新2

--- we-idea/src/main/java/com/example/liu/weidea/config/WebSocketConfig.java
+++ we-idea/src/main/java/com/example/liu/weidea/config/WebSocketConfig.java
@@ -21,67 +21,67 @@ import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    private List<User> users;
-
-    @Autowired
-    UserDao userDao;
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://www.guoruijava.xyz").withSockJS();
-    }
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //表示客户端订阅地址的前缀信息，也就是客户端接收服务端消息的地址的前缀信息
-        registry.enableSimpleBroker("/topic");
-        //指服务端接收地址的前缀，意思就是说客户端给服务端发消息的地址的前缀
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
-    }
-
-    @EventListener
-    public void connectedEvent(SessionConnectedEvent event) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        System.out.println("接入...");
-        event.getTimestamp();
-        Principal principal = event.getUser();
-        Method method = principal.getClass().getDeclaredMethod("getPrincipal");
-        int id = ((User)(method.invoke(event.getUser(),null))).getId();
-        this.users.add(id);
-        System.out.println("由用户接入连接..."+   "当前用户总数" + users.toString());
-    }
-    @EventListener
-    public void onDisconnectEvent(SessionDisconnectEvent event) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        System.out.println("退出...");
-        Principal principal = event.getUser();
-        Method method = principal.getClass().getDeclaredMethod("getPrincipal");
-        int id = ((User)(method.invoke(event.getUser(),null))).getId();
-        System.out.println("退出...");
-        int index=0;
-        Boolean is = false;
-        for (;index<users.size();index++){
-            if (users.get(index) == id){
-                is = true;
-                break;}
-        }
-        if (is)
-            this.users.remove(index);
-        userDao.updateLastDate(new Date(),id);
-        System.out.println("由用户退出连接...当前用户总数" + users.toString());
-    }
-
-
-}
-
 //@Configuration
-//public class WebSocketConfig {
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//    private List<User> users;
+//
+//    @Autowired
+//    UserDao userDao;
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/chat").setAllowedOrigins("http://www.guoruijava.xyz").withSockJS();
+//    }
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        //表示客户端订阅地址的前缀信息，也就是客户端接收服务端消息的地址的前缀信息
+//        registry.enableSimpleBroker("/topic");
+//        //指服务端接收地址的前缀，意思就是说客户端给服务端发消息的地址的前缀
+//        registry.setApplicationDestinationPrefixes("/app");
+//    }
 //    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
+//    public ServerEndpointExporter serverEndpointExporter(){
 //        return new ServerEndpointExporter();
 //    }
+//
+//    @EventListener
+//    public void connectedEvent(SessionConnectedEvent event) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//        System.out.println("接入...");
+//        event.getTimestamp();
+//        Principal principal = event.getUser();
+//        Method method = principal.getClass().getDeclaredMethod("getPrincipal");
+//        int id = ((User)(method.invoke(event.getUser(),null))).getId();
+//        this.users.add(id);
+//        System.out.println("由用户接入连接..."+   "当前用户总数" + users.toString());
+//    }
+//    @EventListener
+//    public void onDisconnectEvent(SessionDisconnectEvent event) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+//        System.out.println("退出...");
+//        Principal principal = event.getUser();
+//        Method method = principal.getClass().getDeclaredMethod("getPrincipal");
+//        int id = ((User)(method.invoke(event.getUser(),null))).getId();
+//        System.out.println("退出...");
+//        int index=0;
+//        Boolean is = false;
+//        for (;index<users.size();index++){
+//            if (users.get(index) == id){
+//                is = true;
+//                break;}
+//        }
+//        if (is)
+//            this.users.remove(index);
+//        userDao.updateLastDate(new Date(),id);
+//        System.out.println("由用户退出连接...当前用户总数" + users.toString());
+//    }
+//
+//
 //}
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
